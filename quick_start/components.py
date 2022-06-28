5b91260912b97c09ec5969e4659cf8df4f0ef712
thomas chaton <thomas@grid.ai>
2022-06-28 11:43:19 +0100

    update

--- quick_start/components.py
+++ quick_start/components.py
@@ -18,51 +18,81 @@ logger = logging.getLogger(__name__)
 class PyTorchLightningScript(TracerPythonScript):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, raise_exception=True, **kwargs)
+        # 1. Keep track of the best model path.
         self.best_model_path = None
-        self._process = None
 
     def configure_tracer(self):
+        # 1. Override `configure_tracer``
+
+        # 2. Import objects from pytorch_lightning
         from pytorch_lightning import Trainer
         from pytorch_lightning.callbacks import Callback
 
+        # 3. Create a tracer.
         tracer = super().configure_tracer()
 
-        class CollectURL(Callback):
+        # 4. Implement a callback to launch tensorboard server.
+        class TensorboardServerLauncher(Callback):
 
             def __init__(self, work):
+                # The provided `work` is the current ``PyTorchLightningScript`` work.
                 self._work = work
 
             def on_train_start(self, trainer, *_):
+                # Provide `host` and `port` in order for tensorboard to be usable in the cloud.
                 self._work._process = Popen(
                     f"tensorboard --logdir='{trainer.logger.log_dir}' --host {self._work.host} --port {self._work.port}",
                     shell=True,
                 )
 
         def trainer_pre_fn(self, *args, work=None, **kwargs):
-            kwargs['callbacks'].append(CollectURL(work))
+            # Intercept Trainer __init__ call and inject a ``TensorboardServerLauncher`` component.
+            kwargs['callbacks'].append(TensorboardServerLauncher(work))
             return {}, args, kwargs
 
-        tracer = super().configure_tracer()
+        # 5. Patch the `__init__` method of the Trainer to inject our callback with a reference to the work.
         tracer.add_traced(Trainer, "__init__", pre_fn=partial(trainer_pre_fn, work=self))
         return tracer
 
     def run(self, *args, **kwargs):
+        ######### [DEMO PURPOSE] #########
+
+        # 1. Download a pre-trained model for speed reason.
         download_data("https://pl-flash-data.s3.amazonaws.com/assets_lightning/demo_weights.pt", "./")
+
+        # 2. Add some arguments to the Trainer to make training faster.
         self.script_args += [
             "--trainer.limit_train_batches=4",
             "--trainer.limit_val_batches=4",
             "--trainer.callbacks=ModelCheckpoint",
             "--trainer.callbacks.monitor=val_acc",
         ]
+
+        # 3. Utilities
         warnings.simplefilter("ignore")
         logger.info(f"Running train_script: {self.script_path}")
+        ######### [DEMO PURPOSE] #########
+
+        logger.info(f"Running train_script: {self.script_path}")
+
+        # 4. Execute the parent run method
         super().run(*args, **kwargs)
 
-    def on_after_run(self, res):
-        lightning_module = res["cli"].trainer.lightning_module
-        checkpoint = torch.load(res["cli"].trainer.checkpoint_callback.best_model_path)
+    def on_after_run(self, script_globals):
+        # 1. Once the script has finished to execute, we can collect its globals and access any objects.
+        # Here, we are accessing the LightningCLI and the associated lightning_module
+        lightning_module = script_globals["cli"].trainer.lightning_module
+
+        # 2. From the checkpoint_callback, we are accessing the best model weights
+        checkpoint = torch.load(script_globals["cli"].trainer.checkpoint_callback.best_model_path)
+
+        # 3. Load the best weights and torchscript the model.
         lightning_module.load_state_dict(checkpoint["state_dict"])
         lightning_module.to_torchscript("model_weight.pt")
+
+        # 4. Use lightning.app.storage.Path to create a reference to the torchscripted model
+        # When running in the cloud on multiple machines, by simply passing this reference to another work,
+        # it triggers automatically a transfer.
         self.best_model_path = Path("model_weight.pt")
 
 class ImageServeGradio(ServeGradio):
@@ -78,8 +108,12 @@ class ImageServeGradio(ServeGradio):
         self._labels = {idx: str(idx) for idx in range(10)}
 
     def run(self, best_model_path):
+        ######### [DEMO PURPOSE] #########
+        # Download some examples so it works locally and in the cloud (issue with gradio on loading the images.)
         download_data("https://pl-flash-data.s3.amazonaws.com/assets_lightning/images.tar.gz", "./")
         self.examples = [os.path.join(str("./images"), f) for f in os.listdir("./images")]
+        ######### [DEMO PURPOSE] #########
+
         self.best_model_path = best_model_path
         self._transform = T.Compose([T.Resize((28, 28)), T.ToTensor()])
         super().run()
